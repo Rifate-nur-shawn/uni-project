6b39fd07a835b2503e7e2af2a249b4c3dc7e24b6
shawn <rnshawn99@gmail.com>
2025-09-23 22:33:51 +0600

    jdk updated into jdk 21

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module com.example.demo1 {
+module com.iamshawn.uniproject {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens com.example.demo1 to javafx.fxml;
-    exports com.example.demo1;
+    opens com.iamshawn.uniproject to javafx.fxml;
+    exports com.iamshawn.uniproject;
 }
